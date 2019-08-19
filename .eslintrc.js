18e1b3a01c15bd16648c955aece18cfe37c79531
mateo <m.devia@uniandes.edu.co>
2019-08-19 09:33:19 -0500

    no-unused-vars eslint rule disabled for event handers

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,22 @@ module.exports = {
         "ecmaVersion": 2018
     },
     "rules": {
-        "no-unused-vars": 0
+        "indent": [
+            "warn",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0
     }
 };
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
-
 let menuEstaAbierto = false;
 
+// I turned off the no unused variables rule because I define event handler function which are never called on the file, but that are called from the HTML files
+
+// eslint-disable-next-line no-unused-vars
 function handleClick() {
   if (!menuEstaAbierto) {
     document.getElementById("navVertical").style.display = "block";
@@ -12,48 +14,49 @@ function handleClick() {
     menuEstaAbierto = false;
   }
 }
-
+// eslint-disable-next-line no-unused-vars
 function homeClicked() {
-  window.location = '/';
+  window.location = "/";
   document.getElementById("formacion1").style.borderBottom = "none";
   document.getElementById("formacion2").style.borderLeft = "none";
   document.getElementById("proyectos1").style.borderBottom = "none";
   document.getElementById("proyectos2").style.borderLeft = "none";
 }
-
+// eslint-disable-next-line no-unused-vars
 function formacionClicked() {
   document.getElementById("formacion1").style.borderBottom = "4px solid";
   document.getElementById("formacion2").style.borderLeft = "8px solid";
   document.getElementById("proyectos1").style.borderBottom = "none";
   document.getElementById("proyectos2").style.borderLeft = "none";
 }
-
+// eslint-disable-next-line no-unused-vars
 function proyectosClicked() {
   document.getElementById("formacion1").style.borderBottom = "none";
   document.getElementById("formacion2").style.borderLeft = "none";
   document.getElementById("proyectos1").style.borderBottom = "4px solid";
   document.getElementById("proyectos2").style.borderLeft = "8px solid";
 }
-
+// eslint-disable-next-line no-unused-vars
 function videoArquisoft() {
   document.getElementById("modalArquisoft").style.display = "block";
 }
-
+// eslint-disable-next-line no-unused-vars
 function cerrarArquisoft() {
   document.getElementById("modalArquisoft").style.display = "none";
 }
-
+// eslint-disable-next-line no-unused-vars
 function videoRegesta() {
   document.getElementById("modalRegesta").style.display = "block";
 }
-
+// eslint-disable-next-line no-unused-vars
 function cerrarRegesta() {
   document.getElementById("modalRegesta").style.display = "none";
 }
+// eslint-disable-next-line no-unused-vars
 function videoDemo() {
   document.getElementById("modalDemo").style.display = "block";
 }
-
+// eslint-disable-next-line no-unused-vars
 function cerrarDemo() {
   document.getElementById("modalDemo").style.display = "none";
 }
